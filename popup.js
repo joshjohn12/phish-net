ae13a778efadd1c7d0be5da35272bacab2fdc10e
Austin Abraham <abaustinva@gmail.com>
2023-10-21 12:21:17 -0500

    loaded extension

---
+++ popup.js
@@ -0,0 +1,18 @@
+// popup.js
+document.addEventListener('DOMContentLoaded', function () {
+    const startMonitoringButton = document.getElementById('startMonitoring');
+    const stopMonitoringButton = document.getElementById('stopMonitoring');
+  
+    // Handle the "Start Monitoring" button click
+    startMonitoringButton.addEventListener('click', function () {
+      // Insert your logic to start email monitoring here
+      // You might want to communicate with background scripts or content scripts to perform this task.
+    });
+  
+    // Handle the "Stop Monitoring" button click
+    stopMonitoringButton.addEventListener('click', function () {
+      // Insert your logic to stop email monitoring here
+      // Communicate with other parts of your extension if necessary.
+    });
+  });
+  
