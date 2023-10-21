b92fc5cdd9595ac9ae73a02c1cb7455d19099be1
Austin Abraham <abaustinva@gmail.com>
2023-10-21 14:55:17 -0500

    motion

--- popup.js
+++ popup.js
@@ -1,18 +1,47 @@
-// popup.js
-document.addEventListener('DOMContentLoaded', function () {
-    const startMonitoringButton = document.getElementById('startMonitoring');
-    const stopMonitoringButton = document.getElementById('stopMonitoring');
-  
-    // Handle the "Start Monitoring" button click
-    startMonitoringButton.addEventListener('click', function () {
-      // Insert your logic to start email monitoring here
-      // You might want to communicate with background scripts or content scripts to perform this task.
+
+let scrapeEmails = document.getElementById('scrapeEmails');
+let list = document.getElementById('emailList');
+
+// Handler to receive emails from content script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // Get emails
+    let emails = request.emails;
+
+    emails.forEach((email) => {
+        let li = document.createElement("li");
+        li.innerText = email;
+        list.appendChild(li);
     });
-  
-    // Handle the "Stop Monitoring" button click
-    stopMonitoringButton.addEventListener('click', function () {
-      // Insert your logic to stop email monitoring here
-      // Communicate with other parts of your extension if necessary.
+
+});
+
+// Button's click event listener
+scrapeEmails.addEventListener("click", async () => {
+    // Get the current active tab
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    // Execute the script to parse emails on the page
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: scrapeEmailsFromPage,
     });
-  });
-  
+});
+
+// Function to scrape emails
+function scrapeEmailsFromPage() {
+    // const emailRegEx = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/g;
+
+    // let emails = document.body.innerHTML.match(emailRegEx);
+
+    // // Send emails to popup
+    // chrome.runtime.sendMessage({ emails });
+    const emailRegEx = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/g;
+
+    let emails = document.body.innerHTML.match(emailRegEx);
+
+    // Filter out duplicates
+    emails = Array.from(new Set(emails));
+
+    // Send unique emails to the popup
+    chrome.runtime.sendMessage({ emails });
+}
